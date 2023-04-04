0679999b99809fea53a0ad2aab735011ff04734c
Clara2605 <bondrescu.clara.h5i@student.ucv.ro>
2023-04-04 23:26:02 +0300

    -create firebase -add activities for login,signup,main,profile,edit,profile -add drawable elements

--- app/src/main/java/com/example/findmyfriends/MainActivity.java
+++ app/src/main/java/com/example/findmyfriends/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.findmyfriends;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView btn = findViewById(R.id.inputButtonLogin2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,SignupActivity.class));
+            }
+        });
+
+        TextView btn2 = findViewById(R.id.inputButtonLogin3);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            }
+        });
     }
 }
